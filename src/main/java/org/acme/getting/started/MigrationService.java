dda9525622c84939229053b9c153ca30a10b6338
SkyName <sky-1999.evgeny@yandex.ru>
2022-02-02 21:46:58 +0300

    commit all new

---
+++ src/main/java/org/acme/getting/started/MigrationService.java
@@ -0,0 +1,20 @@
+package org.acme.getting.started;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.*;
+
+import org.flywaydb.core.Flyway;
+
+
+@ApplicationScoped
+public class MigrationService {
+    // You can Inject the object if you want to use it manually
+    @Inject
+    Flyway flyway; 
+
+    public void checkMigration() {
+        System.out.println(flyway.info().current().getVersion().toString());
+       
+    }
+}
