e73ebdcf2cc3c2c19d0532e121a86aa88c583e62
SkyName <sky-1999.evgeny@yandex.ru>
2022-03-18 12:22:29 +0300

    service, filter, sorter

---
+++ src/main/java/com/vega/service/ContactService.java
@@ -0,0 +1,64 @@
+package com.vega.service;
+
+import com.vega.entities.Contact;
+import com.vega.entities.Vacancy;
+import com.vega.processing.Filter;
+import com.vega.processing.Sorter;
+import com.vega.repositories.ContactRepository;
+import io.quarkus.panache.common.Page;
+import io.quarkus.security.identity.SecurityIdentity;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+@ApplicationScoped
+public class ContactService {
+    @Inject
+    ContactRepository repository;
+
+    public List<Contact> getAll(List<Sorter> sorts, List<Filter> filters, int pageIndex, int pageSize) {
+        Page page = Page.of(pageIndex, pageSize);
+        List<Contact> contacts = repository.findAll(sorts,filters ,page);
+        Contact contact = new Contact();
+        contact.setUserId(SecurityIdentity.USER_ATTRIBUTE);
+        Predicate<Contact> predicateOne = c -> c.getUserId().equals(contact.getUserId());
+        return (List<Contact>) contacts.stream().filter(predicateOne);
+    }
+
+    public Contact get(UUID id) {
+        return repository.findById(id);
+    }
+
+
+    public Boolean delete(UUID id) {
+        return repository.deleteById(id);
+    }
+
+
+    public Contact add(Contact contactToSave) {
+        repository.persist(contactToSave);
+        return repository.findById(contactToSave.getId());
+    }
+
+    public Contact update(UUID id, Contact contactToSave) {
+        Contact contact = repository.findById(id);
+        contact.setFirstName(contactToSave.getFirstName());
+        contact.setLastName(contactToSave.getLastName());
+        contact.setCity(contactToSave.getCity());
+        contact.setCompany(contactToSave.getCompany());
+        contact.setSkype(contactToSave.getSkype());
+        contact.setTelegram(contactToSave.getTelegram());
+        contact.setTelephone(contactToSave.getTelephone());
+        contact.setNotes(contactToSave.getNotes());
+        contact.setMail(contactToSave.getMail());
+        contact.setPosition(contactToSave.getPosition());
+        contact.setVk(contactToSave.getVk());
+        contact.setVacancyId(contactToSave.getVacancyId());
+        repository.persist(contact);
+        return  contact;
+    }
+}
+
---
+++ src/main/java/com/vega/service/EventService.java
@@ -0,0 +1,55 @@
+package com.vega.service;
+
+import com.vega.entities.Event;
+import com.vega.entities.Vacancy;
+import com.vega.processing.Filter;
+import com.vega.processing.Sorter;
+import com.vega.repositories.EventRepository;
+import io.quarkus.panache.common.Page;
+import io.quarkus.security.identity.SecurityIdentity;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+@ApplicationScoped
+public class EventService {
+    @Inject
+    EventRepository repository;
+
+    public List<Event> getAll(List<Sorter> sorts, List<Filter> filters, int pageIndex, int pageSize) {
+        Page page = Page.of(pageIndex, pageSize);
+        List<Event> events = repository.findAll(sorts,filters ,page);
+        Event event = new Event();
+        event.setUserId(SecurityIdentity.USER_ATTRIBUTE);
+        Predicate<Event> predicateOne = ev -> ev.getUserId().equals(event.getUserId());
+        return (List<Event>) events.stream().filter(predicateOne);
+    }
+
+    public Event get(UUID id) {
+        return repository.findById(id);
+    }
+
+    public Boolean delete(UUID id) {
+        return repository.deleteById(id);
+    }
+
+    public Event add(Event eventToSave) {
+        repository.persist(eventToSave);
+        return repository.findById(eventToSave.getId());
+    }
+
+    public Event update(UUID id, Event eventToSave) {
+        Event event = repository.findById(id);
+        event.setName(eventToSave.getName());
+        event.setBeginDate(eventToSave.getBeginDate());
+        event.setEndDate(eventToSave.getEndDate());
+        event.setIsCompleted(eventToSave.getIsCompleted());
+        event.setVacancyId(eventToSave.getVacancyId());
+        repository.persist(event);
+        return  event;
+    }
+}
+
