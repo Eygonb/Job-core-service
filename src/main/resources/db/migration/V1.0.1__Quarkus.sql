5fec1c63e99d4deeb14b30678a13e62229f9a110
Eygonb <30298609+Eygonb@users.noreply.github.com>
2022-05-27 20:39:49 +0300

    Fix db migration

--- src/main/resources/db/migration/V1.0.1__Quarkus.sql
+++ src/main/resources/db/migration/V1.0.1__Quarkus.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-ALTER TABLE statuses DROP CONSTRAINT statuses_pkey,
+ALTER TABLE statuses DROP CONSTRAINT pk_status,
     ADD id uuid not null DEFAULT uuid_generate_v4 (),
     ADD CONSTRAINT pk_status PRIMARY KEY (id),
     ADD CONSTRAINT user_id_name_status_unique UNIQUE (user_id, name_status);
